11cbc472855fe054a60a926b323d175a3c0fe821
leofold4204 <leofold4204@checkupv.com>
2023-09-08 17:07:35 +0900

    feat(components): Change common component folder name

--- src/components/Icon/index.ts
+++
@@ -1,8 +0,0 @@
-import SvgIcon from './SvgIcon.vue'
-
-export * from '@ant-design/icons-vue'
-export { SvgIcon }
-
-// Object.keys(Icons).forEach(key => {
-//   app.component(key, Icons[key as keyof typeof Icons]);
-// });
