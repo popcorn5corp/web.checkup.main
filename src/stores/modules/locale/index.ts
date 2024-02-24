c5b0b58eadee361481bc48c6f0fdc444350281a5
kimnayong <usiyoung@checkupv.com>
2024-02-24 15:57:54 +0900

    fix(storybook): edit store defined hooks format

--- src/stores/modules/locale/index.ts
+++ src/stores/modules/locale/index.ts
@@ -6,30 +6,56 @@ import type { LocaleState } from './types'
 
 const { locales } = Helper.Locale
 
-export let useLocaleStore = defineStore('locale', () => {
-  const state = reactive<LocaleState>({
-    locale: locales.persistedLocale
-  })
+//
+// export const useLocaleStore = defineStore('locale', () => {
+//   const state = reactive<LocaleState>({
+//     locale: locales.persistedLocale
+//   })
+//
+//   const getLocale = computed(() => state.locale || locales.browserLanguage || locales.defaultLocale)
+//
+//   function setLocale(locale: LocaleState['locale']): void {
+//     state.locale = locale
+//     setPersistedLocale(locale)
+//   }
+//
+//   function setPersistedLocale(locale: LocaleState['locale']) {
+//     locales.persistedLocale = locale
+//   }
+//
+//   return {
+//     ...state,
+//     getLocale,
+//     setLocale,
+//     setPersistedLocale
+//   }
+// })
 
-  const getLocale = computed(() => state.locale || locales.browserLanguage || locales.defaultLocale)
-
-  function setLocale(locale: LocaleState['locale']): void {
-    state.locale = locale
-    setPersistedLocale(locale)
-  }
-
-  function setPersistedLocale(locale: LocaleState['locale']) {
-    locales.persistedLocale = locale
-  }
-
-  return {
-    ...state,
-    getLocale,
-    setLocale,
-    setPersistedLocale
-  }
-})
 
 export function useLocaleStoreWithOut() {
+  const useLocaleStore = defineStore('locale', () => {
+    const state = reactive<LocaleState>({
+      locale: locales.persistedLocale
+    })
+
+    const getLocale = computed(() => state.locale || locales.browserLanguage || locales.defaultLocale)
+
+    function setLocale(locale: LocaleState['locale']): void {
+      state.locale = locale
+      setPersistedLocale(locale)
+    }
+
+    function setPersistedLocale(locale: LocaleState['locale']) {
+      locales.persistedLocale = locale
+    }
+
+    return {
+      ...state,
+      getLocale,
+      setLocale,
+      setPersistedLocale
+    }
+  })
+
   return useLocaleStore(store)
 }
